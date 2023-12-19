f079ca7fbb799a402f6866b0b7e96fcc043919e1
Kaitlyn Nowicki <katie.nowicki024@gmail.com>
2023-12-19 10:15:04 -0500

    cc practice

--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -1,19 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PokemonCollection from "./PokemonCollection";
 import PokemonForm from "./PokemonForm";
 import Search from "./Search";
 import { Container } from "semantic-ui-react";
+const url = "http://localhost:3001/pokemon"
 
 function PokemonPage() {
+
+  const [pokemon, setPokemon] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+useEffect(() => {
+  fetch(url)
+  .then((res) => res.json())
+  .then(data => setPokemon(data));
+    },[]);
+
+    function handleAddPokemon(newPokemon) {
+      setPokemon([...pokemon, newPokemon]);
+    }
+
+    console.log(searchTerm)
+
+    const pokemonToDisplay = pokemon.filter((poke) => 
+     poke.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
   return (
     <Container>
       <h1>Pokemon Searcher</h1>
       <br />
-      <PokemonForm />
+      <PokemonForm onAddPokemon={handleAddPokemon}/>
       <br />
-      <Search />
+      <Search 
+      searchTerm = {searchTerm} 
+      onChangeSearch={setSearchTerm}/>
       <br />
-      <PokemonCollection />
+      <PokemonCollection 
+      pokemon={pokemonToDisplay}/>
     </Container>
   );
 }
